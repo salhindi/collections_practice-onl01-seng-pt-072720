83e8429286a845f255692d032cfa5bbd245ae2fd
Sara A <calisaraalhindi@gmail.com>
2020-08-08 19:06:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array [1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect {|arr| arr.delete(arr[2]).insert(2, "$")}
+end
+
+def find_a(array)
+  array.keep_if {|arr| arr[0] == "a"}
+end
+
+def sum_array(array)
+  array.inject(0) {|sum, arr| sum + arr}
+end
+
+def add_s(array)
+  array.each.with_index.collect do |element, index|
+  if index != 1 
+    element = element + "s"
+  else
+    element = element
+  end
+end
+end
